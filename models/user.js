5c8ba68706aba5abf8e3c6569f1405f11c2819d3
joeysk2012 <kurokaw1@gmail.com>
2018-11-21 22:48:46 -0500

    added mongoose population

--- models/user.js
+++ models/user.js
@@ -3,14 +3,13 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
+    firstname: {
+      type: String,
+        default: ''
     },
-    password:  {
-        type: String,
-        required: true
+    lastname: {
+      type: String,
+        default: ''
     },
     admin:   {
         type: Boolean,
